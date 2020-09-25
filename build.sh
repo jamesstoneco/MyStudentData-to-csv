bf1bb69449d9a5211bbbf0008293489ba9f6c775
James Stone <james.stone@futurice.com>
2020-09-25 23:05:17 +0300

    simple grep to get at desired info

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+grep -E 'Loan Type:|Loan Award ID:|Loan Attending School Name:|Loan Date:|Loan Amount:|Loan Disbursed Amount:|Loan Outstanding Principal Balance:|Loan Outstanding Interest Balance:|Loan Interest Rate Type:|Loan Interest Rate:|Loan Contact Name:' MyStudentData.txt
